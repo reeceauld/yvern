1de74ffeae93655667298a0bea808c90def2d7d9
reece <reeceauldclg@gmail.com>
2024-09-01 02:09:49 +0100

    4th

--- js/sideNav.js
+++ js/sideNav.js
@@ -1,13 +1,18 @@
-function openNav() {
+function openNav(){
     document.getElementById("pageSideNav").style.width = "75vw";
     document.getElementById("pageSideNavFooter").style.width = "75vw";
-    setTimeout(showFooter, 250)
+    setTimeout(showFooter, 300)
 }
 function showFooter(){
-    document.getElementById("pageSideNavFooter").style.height = "5vh";
+    document.getElementById("pageSideNavFooter").style.height = "10vh";
 }
 
-function closeNav() {
-    document.getElementById("pageSideNav").style.width = "0";
+function closeNav(){
+
     document.getElementById("pageSideNavFooter").style.height = "0";
+    setTimeout(closeNav2, 100)
+}
+
+function closeNav2(){
+    document.getElementById("pageSideNav").style.width = "0";
 }
